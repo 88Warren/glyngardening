aa6d3fbae1be32fb3a05deabbfddbcdc04be9e41
88warren <warren_laura@yahoo.co.uk>
2024-11-16 19:25:32 +0000

    Personal Details removed

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-// if (process.env.NODE_ENV !== "production") {
-//     require('dotenv').config();
-// }
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
 
 const express = require('express');
-const bodyParser = require('body-parser');
-const { getDefaultSettings } = require('http2');
 const path = require('path');
 const ejsMate = require('ejs-mate');
 const nodemailer = require('nodemailer');
@@ -12,19 +10,17 @@ const smtpTransport = require("nodemailer-smtp-transport")
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
     console.log(`Serving on port ${PORT}`)
 });
 
-//View engine setup
 app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 
-//body Parser replacement Middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-//Additional file directories
 app.set('views', path.join(__dirname, '/views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -33,59 +29,33 @@ app.get('/', (req, res) => {
 })
 
 app.post('/send', (req, res) => {
-        let transporter = nodemailer.createTransport(smtpTransport ({
-            //domain host where website is held
-            service: "gmail",
-            // host: 'stmp.mail.yahoo.com', //smtp.
-            // port: 465,
-            // secure: true,
-            auth:  {
-                user: 'lauramwar88@gmail.com',
-                pass: 'Cy3k5jz7x?'
-            }
-        })
-        );
+    let transporter = nodemailer.createTransport(smtpTransport ({
+        service: "gmail",
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        }
+    }));
 
-        var fname = req.body.fname
-        var lname= req.body.lname
-        var email = req.body.email
-        var phone = req.body.phone
-        var message =req.body.message
+    const mailOptions = {
+        from: process.env.EMAIL_USER,
+        to: process.env.RECIPIENT_EMAIL,
+        subject: `New Contact Form Submission from ${req.body.fname} ${req.body.lname}`,
+        text: `
+            Name: ${req.body.fname} ${req.body.lname}
+            Email: ${req.body.email}
+            Phone: ${req.body.phone}
+            Message: ${req.body.message}
+        `
+    };
 
-        const ejs = require("ejs");
-
-//   ejs.renderFile(__dirname + "/views/home.ejs", { fname: fname, lname: lname, phone: phone }, function (err, data) {
-//     if (err) {
-//         console.log(err);
-//     } else {
-        
-            let mailOptions = {
-                from: 'lauramwar88@gmail.com',
-                to: email,
-                subject: `Message from ${req.body.fname}: ${req.body.lname}: ${req.body.phone}`,
-            text:req.body.message
-            
-        };
-    
-
-        // let mailOptions = {
-        //     from: 'lauramwar88@gmail.com',
-        //     to:req.body.to,
-        //     subject: `Message from ${req.body.fname}: ${req.body.lname}: ${req.body.phone}`,
-        //     text: req.body.message
-        // };
-
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                 console.log("there was an error", error)
-            }
-            console.log('Message %s sent: %s', info.messageId, info.response);
-              
-            // return res.status(200).json({message: "message sent"})
-        });
-    // }
-        //redirects here 
-            res.writeHead(301, { Location: '/' });
-            res.end();
-    })
-// });
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log("Error sending email:", error);
+            res.status(500).redirect('/');
+            return;
+        }
+        console.log('Email sent successfully');
+        res.redirect('/');
+    });
+});
